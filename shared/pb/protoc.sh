be703204aa8b1f246ea80e49008b6695347c8b4f
elfhuv <elfhuv@qq.com>
2024-02-18 16:28:23 +0800

    go work

--- shared/pb/protoc.sh
+++ shared/pb/protoc.sh
@@ -6,6 +6,8 @@
 #     fi
 # done
 
+# go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+
 for file in `ls .`; do
     if [ -f ${file} ] && [ ${file##*.} == "proto" ]; then
       protoc --go_out=./out ${file}
